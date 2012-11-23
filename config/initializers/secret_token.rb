cbd7fa4d4004c93533f61d6e9b9896fb4ec81951
shu0115 <s.matsumoto0115@gmail.com>
2012-11-23 15:11:18 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calem::Application.config.secret_token = '13b846e421d101978d2a4d9f993f7f8ba34468fcdf3811c8d9ccdd2a6033c003d51b53f09bf6f8698ded2486ae10a0cbc62261ff78717acb2da83bc3c24a11fa'
