fc843fc9e5ffe286ff580d0f78640d93a82ba267
shu0115 <s.matsumoto0115@gmail.com>
2014-04-23 20:13:24 +0900

    デフォルトで2ページ目読み込み

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,9 @@ $(function(){
   $('.target_blank a').attr('target' , '_blank');
 
   if ($('#autopager_on').val() == 'true') { autopager(); };
+
+  // 初期状態で2ページ目を読み込む(ディスプレイの縦幅が大きい場合スクロールバーが表示されないため)
+  add_page($(this));
 });
 
 // オートページャー
@@ -33,26 +36,30 @@ function autopager() {
     if (current < $(document).height() - 400) return;  // 下部に達していなければリターン
     if (obj.data('loading')) { return };               // ロード中であればリターン
 
-    // パラメータ
-    var target_month = $('#target_month').val();
-    var next_month   = $('#next_month_' + target_month).val();
-    var page         = parseInt($('#current_page').val());
-    var next_page    = page + 1
-
-    obj.data('loading', true);  // ローディングフラグON
-    $.get(
-      "/schedules/pager",
-      // 送信データ
-      { 'target_month': target_month, 'page': page },
-      function(data, status) {
-        $('#page_' + page).after(data);     // データ追加
-        $('#current_page').val(next_page);  // ページ数更新
-        obj.data('loading', false);         // ローディングフラグOFF
-
-        $('a[rel=tooltip]').tooltip( { html: true, placement: 'bottom', trigger: 'hover' } );
-        $('a[rel=popover]').popover( { html: true, placement: 'bottom', trigger: 'hover' } );
-      },
-      "html"                                // 応答データ形式
-    );
+    add_page(obj);
   });
+}
+
+function add_page(obj) {
+  // パラメータ
+  var target_month = $('#target_month').val();
+  var next_month   = $('#next_month_' + target_month).val();
+  var page         = parseInt($('#current_page').val());
+  var next_page    = page + 1
+
+  obj.data('loading', true);  // ローディングフラグON
+  $.get(
+    "/schedules/pager",
+    // 送信データ
+    { 'target_month': target_month, 'page': page },
+    function(data, status) {
+      $('#page_' + page).after(data);     // データ追加
+      $('#current_page').val(next_page);  // ページ数更新
+      obj.data('loading', false);         // ローディングフラグOFF
+
+      $('a[rel=tooltip]').tooltip( { html: true, placement: 'bottom', trigger: 'hover' } );
+      $('a[rel=popover]').popover( { html: true, placement: 'bottom', trigger: 'hover' } );
+    },
+    "html"                                // 応答データ形式
+  );
 }
