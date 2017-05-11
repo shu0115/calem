22d9fbb477f38a2f0ae6609f49b5b011aae9cd5a
shu0115 <s.matsumoto0115@gmail.com>
2017-05-11 11:22:14 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,33 +1,25 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Calem
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # For Tapp
-    Tapp.config.default_printer = :awesome_print
 
     # TimeZone
     config.time_zone = 'Tokyo'
 
     # Locale
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale     = :ja
     I18n.enforce_available_locales = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
-Calem::Application.routes.draw do
-  resources :schedules do
-    get 'pager', on: :collection
-  end
-
-  # For OmniAuth
-  get "/auth/:provider/callback" => "sessions#callback"
-  get "/auth/failure"            => "sessions#failure"
-  get "/logout"                  => "sessions#destroy", as: :logout
-
-  root to: 'top#index'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
