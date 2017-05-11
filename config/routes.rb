9e5b8dc496a74ee658406dd83a589d5f82a97679
shu0115 <s.matsumoto0115@gmail.com>
2017-05-11 11:44:08 +0900

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :schedules do
+    get 'pager', on: :collection
+  end
+
+  # For OmniAuth
+  get "/auth/:provider/callback" => "sessions#callback"
+  get "/auth/failure"            => "sessions#failure"
+  get "/logout"                  => "sessions#destroy", as: :logout
+
+  root to: 'top#index'
 end
